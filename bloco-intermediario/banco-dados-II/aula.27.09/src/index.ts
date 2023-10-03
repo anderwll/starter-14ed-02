d544469043b2831654fe769842e157bb9a86fa43
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-10-02 22:35:13 -0300

    initial config node ts prisma

--- bloco-intermediario/banco-dados-II/aula.27.09/src/index.ts
+++ bloco-intermediario/banco-dados-II/aula.27.09/src/index.ts
@@ -1,1 +1,7 @@
 console.log("Hello Word !!");
+
+const data = [{product_name: '', ...}, {product_name: '', ...}, {product_name: '', ...}];
+
+app.get('/received', (req, res) => {
+    res.status(200).json({success: true, message: "Ok", data})
+})
