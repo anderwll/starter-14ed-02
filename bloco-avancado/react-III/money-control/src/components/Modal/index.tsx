7fdbcb56f7f42382e90d342b72fb9211c39b259c
FelipeGiehl <giehl.felipe@gmail.com>
2023-11-20 22:18:29 -0300

    create componet modal

---
+++ bloco-avancado/react-III/money-control/src/components/Modal/index.tsx
@@ -0,0 +1,86 @@
+import {
+    Box,
+    Button,
+    FormControl,
+    Grid,
+    InputLabel,
+    MenuItem,
+    Modal as ModalMui,
+    Select,
+    TextField,
+    Typography,
+} from "@mui/material";
+
+const style = {
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+interface ModalProps{
+    open:boolean
+    handleClose: () => void
+}
+
+function Modal(props:ModalProps) {
+
+  return (
+    <>
+      <ModalMui
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Adicionar Transação
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="value"
+                label="Valor"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel id="label-type">Tipo</InputLabel>
+                <Select labelId="label-type" id="type" label="Tipo">
+                  <MenuItem value={"income"}>Entrada</MenuItem>
+                  <MenuItem value={"outcome"}>Saída</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="description"
+                label="Descrição"
+                variant="outlined"
+                multiline
+                rows={4}
+              />
+            </Grid>
+            <Grid item xs={12} display={"flex"} justifyContent={"end"} gap={1}>
+              <Button variant="text" color="inherit">Cancelar</Button>
+              <Button variant="contained">Cadastrar</Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </ModalMui>
+    </>
+  );
+}
+
+export default Modal;
--- bloco-avancado/react-III/money-control/src/pages/Home.tsx
+++ bloco-avancado/react-III/money-control/src/pages/Home.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import AppBar from "../components/AppBar";
 import FloatButton from "../components/FloatButton";
+import Modal from "../components/Modal";
+
 
 function Home() {
+
+  const [open,setOpen] = useState(false)
+
+  function handleModal(){
+    setOpen(!open)
+  }
+
   return (
     <>
       <AppBar />
-      <FloatButton handleClick={ () => alert('Abrir modal!')}/>
+      <FloatButton handleClick={ handleModal } />
+      <Modal open={open} handleClose={handleModal} />
     </>
   );
 }
