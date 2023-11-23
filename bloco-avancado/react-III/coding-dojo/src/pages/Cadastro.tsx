3a0d250baad97c51e7534e7da8bb38b7ebc2f88c
Pietro Bilialva <pietrobilialva@hotmail.com>
2023-11-22 21:22:44 -0300

    ajust overflow

--- bloco-avancado/react-III/coding-dojo/src/pages/Cadastro.tsx
+++ bloco-avancado/react-III/coding-dojo/src/pages/Cadastro.tsx
@@ -11,17 +11,17 @@ import FormCadastro from "../components/FormCadastro";
 
 function Cadastro() {
   return (
-      <Grid container>
+      <Grid container overflow="hidden">
         <Grid item md={8}>
           <img
             alt="Imagem de praia"
-            src="https://initiate.alphacoders.com/images/134/cropped-1920-1080-1341852.png?1410"
+            src="./public/maos.jpg"
             height="100%"
-            width='100%'
+            width="100%"
           />
         </Grid>
         <Grid item md={4} display="flex">
-          <Box component="section" display="flex" alignItems="center">
+          <Box component="section" display="flex" alignItems="center" height='100vh'>
             <Container maxWidth="lg">
               <Stack textAlign="center" justifyContent="center" m={2}>
                 <Box>
@@ -29,11 +29,7 @@ function Cadastro() {
                 </Box>
                 <Typography variant="h5">Cadastrar</Typography>
                 <FormCadastro />
-                <Link
-                  color="inherit"
-                  href="https://www.warnerbros.com.br/"
-                  m={1}
-                >
+                <Link color="inherit" href="#" m={1}>
                   Você já possui conta? Vá para login.
                 </Link>{" "}
                 <Typography
