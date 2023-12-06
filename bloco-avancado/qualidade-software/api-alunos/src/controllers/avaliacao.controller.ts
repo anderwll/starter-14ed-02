8e471b5d2a7fa968776fa532d83ffad5bb3ad40e
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-12-06 19:03:31 -0300

    adjust env example

--- bloco-avancado/qualidade-software/api-alunos/src/controllers/avaliacao.controller.ts
+++ bloco-avancado/qualidade-software/api-alunos/src/controllers/avaliacao.controller.ts
@@ -1,91 +1,96 @@
-import { Request, Response } from 'express';
-import { AvaliacaoService } from '../services';
+import { Request, Response } from "express";
+import { AvaliacaoService } from "../services";
 
 export class AvaliacaoController {
-	public async cadastrar(req: Request, res: Response) {
-		try {
-			const { modulo, nota, idAluno } = req.body;
+  public async cadastrar(req: Request, res: Response) {
+    try {
+      const { modulo, nota, idAluno } = req.body;
 
-			const service = new AvaliacaoService();
-			const response = await service.criar({ modulo, nota, idAluno });
+      const service = new AvaliacaoService();
+      const response = await service.criar({ modulo, nota, idAluno });
 
-			return res.status(response.code).json(response);
-		} catch (error: any) {
-			return res.status(500).json({
-				code: 500,
-				ok: false,
-				mensagem: error.toString(),
-			});
-		}
-	}
+      return res.status(response.code).json(response);
+    } catch (error: any) {
+      return res.status(500).json({
+        code: 500,
+        ok: false,
+        mensagem: error.toString(),
+      });
+    }
+  }
 
-	public async listar(req: Request, res: Response) {
-		try {
-			const { aluno } = req.query; // ?aluno="124564-124545fgd-fdsgdfg-dfgdf" | undefined
-			const service = new AvaliacaoService();
-			const response = await service.listarTodos(aluno as string | undefined);
+  public async listar(req: Request, res: Response) {
+    try {
+      const { aluno } = req.query;
+      const service = new AvaliacaoService();
+      const response = await service.listarTodos(aluno as string | undefined);
 
-			return res.status(response.code).json(response);
-		} catch (error: any) {
-			return res.status(500).json({
-				code: 500,
-				ok: false,
-				mensagem: error.toString(),
-			});
-		}
-	}
+      return res.status(response.code).json(response);
+    } catch (error: any) {
+      return res.status(500).json({
+        code: 500,
+        ok: false,
+        mensagem: error.toString(),
+      });
+    }
+  }
 
-	public async listarPorID(req: Request, res: Response) {
-		try {
-			const { idAluno } = req.body;
-			const { id } = req.params;
+  public async listarPorID(req: Request, res: Response) {
+    try {
+      const { idAluno } = req.body;
+      const { id } = req.params;
 
-			const service = new AvaliacaoService();
-			const response = await service.listarPorID({ idAluno, idAvaliacao: id });
+      const service = new AvaliacaoService();
+      const response = await service.listarPorID({ idAluno, idAvaliacao: id });
 
-			return res.status(response.code).json(response);
-		} catch (error: any) {
-			return res.status(500).json({
-				code: 500,
-				ok: false,
-				mensagem: error.toString(),
-			});
-		}
-	}
+      return res.status(response.code).json(response);
+    } catch (error: any) {
+      return res.status(500).json({
+        code: 500,
+        ok: false,
+        mensagem: error.toString(),
+      });
+    }
+  }
 
-	public async atualizar(req: Request, res: Response) {
-		try {
-			const { idAluno, modulo, nota } = req.body;
-			const { id } = req.params;
+  public async atualizar(req: Request, res: Response) {
+    try {
+      const { idAluno, modulo, nota } = req.body;
+      const { id } = req.params;
 
-			const service = new AvaliacaoService();
-			const response = await service.atualizar({ idAluno, idAvaliacao: id, modulo, nota });
+      const service = new AvaliacaoService();
+      const response = await service.atualizar({
+        idAluno,
+        idAvaliacao: id,
+        modulo,
+        nota,
+      });
 
-			return res.status(response.code).json(response);
-		} catch (error: any) {
-			return res.status(500).json({
-				code: 500,
-				ok: false,
-				mensagem: error.toString(),
-			});
-		}
-	}
+      return res.status(response.code).json(response);
+    } catch (error: any) {
+      return res.status(500).json({
+        code: 500,
+        ok: false,
+        mensagem: error.toString(),
+      });
+    }
+  }
 
-	public async deletar(req: Request, res: Response) {
-		try {
-			const { idAluno } = req.body;
-			const { id } = req.params;
+  public async deletar(req: Request, res: Response) {
+    try {
+      const { idAluno } = req.body;
+      const { id } = req.params;
 
-			const service = new AvaliacaoService();
-			const response = await service.deletar({ idAluno, idAvaliacao: id });
+      const service = new AvaliacaoService();
+      const response = await service.deletar({ idAluno, idAvaliacao: id });
 
-			return res.status(response.code).json(response);
-		} catch (error: any) {
-			return res.status(500).json({
-				code: 500,
-				ok: false,
-				mensagem: error.toString(),
-			});
-		}
-	}
+      return res.status(response.code).json(response);
+    } catch (error: any) {
+      return res.status(500).json({
+        code: 500,
+        ok: false,
+        mensagem: error.toString(),
+      });
+    }
+  }
 }
