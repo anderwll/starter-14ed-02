73d07e7bd678195789ffd45c05df50fbec0fe0e4
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-12-20 22:07:49 -0300

    formated archive

--- bloco-avancado/qualidade-software/api-controle-academico/tests/models/aluno.model.spec.ts
+++ bloco-avancado/qualidade-software/api-controle-academico/tests/models/aluno.model.spec.ts
@@ -1,47 +1,48 @@
-import { ETipoAluno } from "../../src/enum/aluno.enum"
-import { Aluno } from "../../src/models/aluno.model"
+import { ETipoAluno } from "../../src/enum/aluno.enum";
+import { Aluno } from "../../src/models/aluno.model";
 
-describe("Aluno Model", ()=> {
+describe("Aluno Model", () => {
+  const createSut = () => {
+    return new Aluno(
+      "any_id",
+      "any_nome",
+      "any_email",
+      "any_senha",
+      ETipoAluno.M,
+      23
+    );
+  };
 
-    const createSut = ()=>{
-        return new Aluno( "any_id", "any_nome", "any_email", "any_senha", ETipoAluno.M, 23)
-    }
+  afterAll(() => {
+    console.log("executou ---");
+  });
 
-    afterAll(()=>{
-        console.log("executou ---")
-    })
+  test("deve retornar o id do aluno criado", () => {
+    const sut = createSut();
 
-    test("deve retornar o id do aluno criado", () => {
-        const sut = createSut()
-        
-        const result = sut.id
+    const result = sut.id;
 
+    expect(result).toBeDefined();
+    expect(result).toBe("any_id");
+  });
 
-        expect(result).toBeDefined()
-        expect(result).toBe("any_id")
+  it("deve retornar todas as informações menos a senha do aluno criado", () => {
+    const sut = createSut();
 
-    })
+    const result = sut.toJSON();
 
-    it("deve retornar todas as informações menos a senha do aluno criado",()=>{
-         const sut = createSut()
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty("id", "any_id");
+    expect(result).toHaveProperty("nome", "any_nome");
+    expect(result).toHaveProperty("email");
 
-
-
-         const result = sut.toJSON()
-
-         expect(result).toBeDefined()
-         expect(result).toHaveProperty("id","any_id")
-         expect(result).toHaveProperty("nome","any_nome")
-         expect(result).toHaveProperty("email")
-
-         expect(result).toStrictEqual({
-           id: "any_id",
-           nome: "any_nome",
-           email: "any_email",
-           idade: 23,
-           tipo: ETipoAluno.M,
-            endereco: undefined,
-         });
-    })
-
-})
+    expect(result).toStrictEqual({
+      id: "any_id",
+      nome: "any_nome",
+      email: "any_email",
+      idade: 23,
+      tipo: ETipoAluno.M,
+      endereco: undefined,
+    });
+  });
+});
